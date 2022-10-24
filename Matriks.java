ed439bee7097535833f31e5c72034833e67592ed
Caesar W <wicaksonocaesar@gmail.com>
2022-10-24 17:58:07 +0700

    add repo matriks

---
+++ Matriks.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+
+public class Matriks {
+    public static void main(String[] args) {
+        int[][] arr = new int[2][2];
+        int[][] arr2 = new int[2][2];
+        int[][] result = new int[2][2];
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.println("Memasukkan Nilai Matriks 1");
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr.length; j++) {
+                        System.out.print("Masukkan index " + "[" + i + "]" + "[" + j + "] : ");
+                        arr[i][j] = input.nextInt();
+                        if (i == 1 && j == 1) {
+                            System.out.println("\nMasukkan Nilai Matriks 2");
+                            for (int k = 0; k < arr2.length; k++) {
+                                for (int l = 0; l < arr2.length; l++) {
+                                    System.out.print("Masukkan index " + "[" + k + "]" + "[" + l + "] : ");
+                                    arr2[k][l] = input.nextInt();
+                                    result[k][l] = arr[k][l] + arr2[k][l];
+                                    if (k == 1 && l == 1) {
+                                        System.out.println("\nHasil Pejumlahan Matriks Ordo 2x2");
+                                        for (int l2 = 0; l2 < result.length; l2++) {
+                                            for (int m = 0; m < result.length; m++) {
+                                                System.out.print(result[l2][m] + " ");
+                                            }
+                                            System.out.println();
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getCause());
+        }
+    }
+}
